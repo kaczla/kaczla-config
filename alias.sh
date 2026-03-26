7677bb6c4872c0bcd7b821743758deacc370619b
Karol Kaczmarek <kaczla@gmail.com>
2026-03-26 09:10:59 +0100

    Add git repack alias

--- alias.sh
+++ alias.sh
@@ -1,2 +1,3 @@
 alias update_kk='sudo pacman -Syu && flatpak update && flatpak uninstall --unused && yay -Syu --aur'
 alias git_prune='git gc --prune=now && git gc --aggressive --prune=now'
+alias git_repack='git repack -a -d -f --depth=250 --window=250'
